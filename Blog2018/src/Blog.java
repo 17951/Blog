89ac0a4fe6013aaf7c109882139bec979676fa71
Mateusz Mórawski <17951@student.pwsz.elblag.pl>
2018-03-12 22:23:11 +0100

    v1

--- Blog2018/src/Blog.java
+++ Blog2018/src/Blog.java
@@ -4,84 +4,59 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Blog {
-    private List<Note> notes = new ArrayList();
-    private String title;
+	private List<Note> notes = new ArrayList();
+	private String title;
 
-    public Blog(String title) {
-        this.title = title;
-    }    
-    
-    public void add(Note note)
-    {
-        notes.add(note);
-    }
-    
-    public String toString()
-    {
-        return title + "\n\n"+notes;
-    }
-    
-    public Note mostPopular()
-    {
-        if(notes.isEmpty()) return null;
-        Note result = notes.get(0);
-        for(Note note : notes)
-        {
-            int count = note.numberOfComments();
-            if(count > result.numberOfComments())
-            {
-                result = note;
-            }
-        }
-        return result;
-        
-    }
-    public List<Note> mostPopulars()
-    {
-        
-        return null;
-    }
-    public Comment lastComment()
-    {
-        List<Comment> allComments = new ArrayList();
-        for(Note note : notes)
-        {
-            allComments.addAll(note.getComments());
-        }
-        Comment result = allComments.get(0);
-        for(Comment comment : allComments)
-        {
-            LocalDate d1 = comment.getDate();
-            LocalDate d2 = result.getDate();
-            if(d1.isAfter(d2))
-            {
-                result = comment;
-            }
-        }
-        
-        
-        /*
-        LocalDate now = LocalDate.now();
-        Comment result = null;
-        long min = Long.MAX_VALUE;
-        for(Note note : notes)
-        {
-            for(Comment comment : note.getComments())
-            {
-                LocalDate date = comment.getDate();
-                long x = now.until(date, ChronoUnit.DAYS);
-                if(x<min)
-                {
-                    min = x;
-                    result = comment;
-                }
-            }
-        }*/
-        return result;
-    }
-    public List<Comment> lastComments()
-    {
-        return null;
-    }
+	public Blog(String title) {
+		this.title = title;
+	}
+
+	public void add(Note note) {
+		notes.add(note);
+	}
+
+	public String toString() {
+		return title + "\n\n" + notes;
+	}
+
+	public Note mostPopular() {
+		if (notes.isEmpty())
+			return null;
+		Note result = notes.get(0);
+		for (Note note : notes) {
+			int count = note.numberOfComments();
+			if (count > result.numberOfComments()) {
+				result = note;
+			}
+		}
+		return result;
+
+	}
+
+	public List<Note> mostPopulars() {
+
+		return null;
+	}
+
+	public Comment lastComment() {
+		List<Comment> allComments = new ArrayList();
+		for (Note note : notes) {
+			allComments.addAll(note.getComments());
+		}
+		Comment result = allComments.get(0);
+		for (Comment comment : allComments) {
+			LocalDate d1 = comment.getDate();
+			LocalDate d2 = result.getDate();
+			if (d1.isAfter(d2)) {
+				result = comment;
+			}
+		}
+
+		return result;
+	}
+
+	public List<Comment> lastComments() {
+		return null;
+	}
 
 }
