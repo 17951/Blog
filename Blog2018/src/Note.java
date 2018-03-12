187db1209e99b2bd18a01566cd07832927c4ccc8
Mateusz Mórawski <17951@student.pwsz.elblag.pl>
2018-03-12 22:13:49 +0100

    Note

---
+++ Blog2018/src/Note.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Note {
+    private String title, content;
+    private List<Comment> comments = new ArrayList();
+
+    public Note(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+    
+    public void add(Comment comment)
+    {
+        comments.add(comment);
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+    
+    public String toString()
+    {
+        StringBuilder sb = new StringBuilder();
+        
+        sb.append("\n\ntytu³: "+title);
+        sb.append("\ntreœæ: "+content);
+        sb.append("\nkomentarze: "+comments);
+        sb.append("\n");
+        return sb.toString();
+    }
+
+    public int numberOfComments() {
+        return comments.size();
+    }
+}
